d2a6cbf10cf071e4e556498bd0ac7b8b3fa41275
Wes Alvaro <git@wesalvaro.com>
2019-03-10 17:23:34 +0900

    Make path translucent

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const watch = async (it, startCoords) => {
 const mapStyle = 'element:geometry.fill%7Cvisibility:off&style=feature:administrative%7Ccolor:0xff0302%7Cvisibility:off&style=feature:administrative.locality%7Celement:geometry.fill%7Cvisibility:on%7Cweight:4.5&style=feature:administrative.locality%7Celement:labels.text%7Ccolor:0x000000%7Cvisibility:on%7Cweight:1&style=feature:landscape%7Cvisibility:off&style=feature:poi%7Cvisibility:off&style=feature:road%7Celement:geometry.stroke%7Ccolor:0x000000&style=feature:road%7Celement:labels.icon%7Ccolor:0x000000%7Cvisibility:off&style=feature:road%7Celement:labels.text%7Cvisibility:off&style=feature:transit%7Celement:labels.icon%7Cvisibility:off&style=feature:transit%7Celement:labels.text%7Cvisibility:off&style=feature:transit.line%7Celement:geometry.fill%7Ccolor:0xff0000%7Cvisibility:on%7Cweight:4.5&style=feature:transit.line%7Celement:geometry.stroke%7Ccolor:0x000000%7Cvisibility:on%7Cweight:4.5&style=feature:transit.station%7Cvisibility:off&style=feature:water%7Celement:geometry.fill%7Ccolor:0x000000%7Cvisibility:on';
 
 const getMapUrl = ({ start, dest, width = 600, height = 448 }) =>
-  `https://maps.googleapis.com/maps/api/staticmap?scale=2&size=${width}x${height}&maptype=roadmap&markers=color:0xff0000|size:small|${dest}&path=color:0xff0000ff|weight:5|${start}|${dest}&key=${key}&style=${mapStyle}`;
+  `https://maps.googleapis.com/maps/api/staticmap?scale=2&size=${width}x${height}&maptype=roadmap&markers=color:0xff0000|size:small|${dest}&path=color:0xff000080|weight:5|${start}|${dest}&key=${key}&style=${mapStyle}`;
 
 let watchIt = null;
 const maximumAge = 30000;
